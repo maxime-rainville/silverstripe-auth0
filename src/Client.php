48582d1e26e89596b41c1bfa6748761714bb76d7
Maxime Rainville <maxime@silverstripe.com>
2018-06-22 20:13:23 +1200

    :white_check_mark: Add test case for our custom client.

--- src/Client.php
+++ src/Client.php
@@ -17,7 +17,8 @@ use SilverStripe\Control\Controller;
  * * AUTH0_CLIENT_ID
  * * AUTH0_CLIENT_SECRET
  *
- * Those values can be retrieve from your Auth0 dashboard.
+ * Those values can be retrieved from your Auth0 dashboard. Alternatively, you can pass them via the `$baseSettings`
+ * parameter on the constructor.
  *
  * If you want to implement your own Auth0 client, you can add this to your YML config to reference your own class.
  * ```YML
@@ -44,13 +45,31 @@ class Client extends Auth0
     public $AuthenticatorKey;
 
     /**
+     * Base settings, will be set via the environment settings most of the time.
+     * @var array
+     */
+    protected $baseSettings = [];
+
+    /**
+     * Base settings, will be set via the environment settings most of the time.
+     * @var string
+     */
+    protected $baseDomain = '';
+
+    /**
      * Instantiate a new Auth0 client.
      * @param string $AuthenticatorKey Key used to add our AUth0 Authenticator to Security.
+     * @param array $baseSetting Optional list of argument to pass to the aut0 client. Can be provided via
+     * environment variables as well.
+     * @param string $domain Auth0 domain where we will connect. Can be provided via the AUTH0_DOMAIN environment
+     * variable as well.
      */
-    public function __construct($AuthenticatorKey='')
+    public function __construct($AuthenticatorKey='', $baseSettings = [], $baseDomain = '')
     {
         $this->AuthenticatorKey = $AuthenticatorKey;
-        return parent::__construct($this->getDefaultSettings());
+        $this->baseSettings = $baseSettings;
+        $this->baseDomain = $baseDomain ?: Environment::getEnv('AUTH0_DOMAIN');
+        return parent::__construct($this->getSettings());
     }
 
     /**
@@ -58,10 +77,10 @@ class Client extends Auth0
      *
      * @return array
      */
-    protected function getDefaultSettings()
+    public function getSettings()
     {
         $domain = $this->getDomain();
-        return [
+        return array_merge([
             'domain' => $domain,
             'client_id' => Environment::getEnv('AUTH0_CLIENT_ID'),
             'client_secret' => Environment::getEnv('AUTH0_CLIENT_SECRET'),
@@ -72,7 +91,7 @@ class Client extends Auth0
             'persist_access_token' => true,
             'persist_refresh_token' => true,
             'store' => false,
-        ];
+        ], $this->baseSettings);
     }
 
     /**
@@ -81,7 +100,7 @@ class Client extends Auth0
      */
     protected function getDomain()
     {
-        $domain = Environment::getEnv('AUTH0_DOMAIN');
+        $domain = $this->baseDomain;
 
         // Append auth0.com to domain
         if (!preg_match('/\.auth0\.com$/i', $domain)) {
